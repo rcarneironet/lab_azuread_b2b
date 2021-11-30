67f9a7c9be6f0ee071ac3b1cc77942cd5bdf25fe
Ray Carneiro <ray.silva@msn.com>
2021-11-30 13:04:09 -0300

    start

---
+++ AADB2BNetCoreMvc/AADB2BNetCoreMvc/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using AADB2BNetCoreMvc.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace AADB2BNetCoreMvc.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult SignOut()
+        {
+            var scheme = OpenIdConnectDefaults.AuthenticationScheme ?? string.Empty;
+            return View(SignOut(new AuthenticationProperties
+            {
+                RedirectUri = "/Home/Index",
+            },
+            CookieAuthenticationDefaults.AuthenticationScheme, scheme));
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
